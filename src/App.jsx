329f3fa956519b23efe78f8d44865a7483c471d8
Yushi <yushari@seznam.cz>
2026-07-07 18:52:59 +0200

    imported dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header.jsx";
+import Dashboard from "./features/dashboard/Dashboard.jsx";
 
 import "./styles/Global.css";
 
@@ -20,6 +21,7 @@ function App() {
   return (
     <div className="app__container">
       <Header theme={theme} onToggleTheme={toggleTheme} />
+      <Dashboard />
     </div>
   );
 }
